1c90e511efab0b42d132f6285e5fe741b7c864f2
sunxianlei <sunxianlei@foxmail.com>
2013-11-24 21:50:23 +0800

    use heatmap.js show heatmap by two points

--- js/map.js
+++ js/map.js
@@ -4,6 +4,7 @@ dojo.addOnLoad(init);
 var map;
 
 function init() {
+	/*
 	var initExtent = new esri.geometry.Extent({
 		xmax: -13624229.32056175,
 		xmin: -13625120.886837104,
@@ -13,36 +14,67 @@ function init() {
 			"wkid": 102100
 		}
 	});
+	*/
 	map = new esri.Map("map", { 
-	  //center: [118.763903, 32.058586],
-	  extent: initExtent,
+	  center: [118.763903, 32.058586],
+	  //extent: initExtent,
 	  zoom: 12,
 	  slider: true,
 	  sliderPosition:'bottom-left'
 	});
-	//var basemapURL= "http://cache1.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineStreetColor/MapServer"   
-	var basemapURL="http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer";
+	var basemapURL= "http://cache1.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineStreetColor/MapServer"   
+	//var basemapURL="http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer";
 	var basemap = new esri.layers.ArcGISTiledMapServiceLayer(basemapURL);   
 	map.addLayer(basemap); 
 	
-	var heatLayer = new HeatmapLayer({
-	"map": map,
-	"domNodeId": "heatLayer",
-	"opacity": 0.85
+	// once map is loaded
+	dojo.connect(map, 'onLoad', function (theMap) {
+
+		dojo.connect(dijit.byId('map'), 'resize', map, map.resize);
+		heatLayer = new HeatmapLayer({
+			"map": map,
+			"domNodeId": "heatLayer",
+			"opacity": 0.85
+		});
+		map.addLayer(heatLayer);
+		map.resize();
+
+		setDatas();//
+
+		dojo.connect(map, "onExtentChange", function (extent) {
+			setDatas();//
+		});
 	});
+}
+function setDatas() {
+	
 	var data = [
 	{
 		attributes: {
-		count:50
+		count:30
 		},
 		geometry: {
 		spatialReference: {wkid: 102100},
 		type: "point",
-		x: -13625196.784400001,
-		y: 4547752.612899996
+		//x: -13625196.784400001,
+		//y: 4547752.612899996
+		x: 13224914.8088105,
+		y: 3765607.09725
+		}
+	},
+	{
+		attributes: {
+		count:20
+		},
+		geometry: {
+		spatialReference: {wkid: 102100},
+		type: "point",
+		//x: -13625196.784400001,
+		//y: 4547752.612899996
+		x: 13224115.8088105,
+		y: 3764608.09725
 		}
 	}
-	];
+	];	
 	heatLayer.setData(data);
-	map.addLayer(heatLayer);
 }
