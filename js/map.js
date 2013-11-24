4888b1179322540ea777c37d9b7b710bf3487276
sunxianlei <sunxianlei@foxmail.com>
2013-11-24 21:24:05 +0800

    add heatmap.js downloaded from Internet

--- js/map.js
+++ js/map.js
@@ -2,14 +2,47 @@ dojo.require("esri.map");
 dojo.addOnLoad(init);
 
 var map;
+
 function init() {
+	var initExtent = new esri.geometry.Extent({
+		xmax: -13624229.32056175,
+		xmin: -13625120.886837104,
+		ymax: 4548374.604660432,
+		ymin: 4547966.144290476,
+		"spatialReference": {
+			"wkid": 102100
+		}
+	});
 	map = new esri.Map("map", { 
-	  center: [118.763903, 32.058586],
+	  //center: [118.763903, 32.058586],
+	  extent: initExtent,
 	  zoom: 12,
 	  slider: true,
 	  sliderPosition:'bottom-left'
 	});
-	var basemapURL= "http://cache1.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineStreetColor/MapServer"   
+	//var basemapURL= "http://cache1.arcgisonline.cn/ArcGIS/rest/services/ChinaOnlineStreetColor/MapServer"   
+	var basemapURL="http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer";
 	var basemap = new esri.layers.ArcGISTiledMapServiceLayer(basemapURL);   
 	map.addLayer(basemap); 
+	
+	var heatLayer = new HeatmapLayer({
+	"map": map,
+	"domNodeId": "heatLayer",
+	"opacity": 0.85
+	});
+	var data = [
+	{
+		attributes: {
+		count:50
+		},
+		geometry: {
+		spatialReference: {wkid: 102100},
+		type: "point",
+		x: -13625196.784400001,
+		y: 4547752.612899996
+		}
+	}
+	];
+	heatLayer.setData(data);
+	map.addLayer(heatLayer);
 }
